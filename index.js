26cc88de9192f494785c1ccbd9294fde20b9f9bd
Nataliia <natali_94@ukr.net>
2022-08-24 16:32:04 +0200

    add file

---
+++ index.js
@@ -0,0 +1,22 @@
+function getName () {
+  const email = prompt('Enter your e-mail');
+  const checkEmail = email.indexOf('admin@test.io');
+
+  if (checkEmail === -1) {
+    alert(`Incorrect E-Mail address`);
+    return;
+  }
+  
+  const password = prompt('Enter your password');
+  const checkPass = password.indexOf('admin1234');
+  
+  if (checkPass === -1) {
+    alert(`Invalid password`);
+  }
+  else {
+    alert(`Successful login`);
+  }
+}
+
+getName ();
+
